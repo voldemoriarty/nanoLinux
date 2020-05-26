07ec85cfa09c109c303c2c748acdfe151d6c2b2a
voldemoriarty <mamin.bee16seecs@seecs.edu.pk>
2020-05-26 15:24:51 +0500

    slight restructure

---
+++ sdimg.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo ./make_sdimage_p3.py -f \
+-P $BOOTLOADERDIR/$BINDIR/u-boot-with-spl.sfp,num=3,format=raw,size=10M,type=A2  \
+-P sdfs,num=1,format=vfat,size=200M \
+-s 500M \
+-n sdcard_cv.img
