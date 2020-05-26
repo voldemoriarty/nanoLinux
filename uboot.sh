7c5e3f9b08e0812a125971d2b3a8ce1ca040ec17
M Saad Bin Amin <mamin.bee16seecs@seecs.edu.pk>
2020-05-26 10:46:52 +0500

    add bootlaoder script

---
+++ uboot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# this script is meant to be run after the hardware
+# is generated. Takes the handoff files from the qsys
+# generated files and compiles the latest release version
+# of altera socfpga uboot
+
+mkdir -p bootloader
