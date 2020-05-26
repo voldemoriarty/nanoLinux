85f14022f8d4960d5acfea9820351b70b92fcba1
M Saad Bin Amin <mamin.bee16seecs@seecs.edu.pk>
2020-05-26 12:36:58 +0500

    update script to build uboot

--- uboot.sh
+++ uboot.sh
@@ -5,4 +5,54 @@
 # generated files and compiles the latest release version
 # of altera socfpga uboot
 
-mkdir -p bootloader
+set -e 
+
+HWDIR=hardware
+HANDOFFDIR=hps_isw_handoff/soc_system_hps
+BOOTLOADERDIR=bootloader
+SETTINGS=settings.bsp
+QTSFILTER=$BOOTLOADERDIR/uboot/arch/arm/mach-socfpga/qts-filter.sh
+BOARD=terasic/de10-nano  
+UBOOT_CFG=socfpga_de10_nano_defconfig
+
+# the path of the cross compiler
+# make sure this is it
+export CROSS_COMPILE=/usr/bin/arm-linux-gnueabihf-
+
+# check if bootloader directory exists
+# if it does, assume these commands have already been run
+if [ ! -d "$BOOTLOADERDIR/uboot" ]; then 
+  # create directory for uboot
+  mkdir -p $BOOTLOADERDIR/uboot
+
+  # download uboot, don't download the history
+  git clone https://github.com/altera-opensource/u-boot-socfpga --depth=1 $BOOTLOADERDIR/uboot
+else 
+  echo "Bootloader directory exists, skipping download ..."
+fi 
+
+# create the settings from the Qsys generated handoff
+bsp-create-settings \
+  --type spl \
+  --bsp-dir $BOOTLOADERDIR \
+  --preloader-settings-dir $HWDIR/$HANDOFFDIR \
+  --settings $BOOTLOADERDIR/$SETTINGS
+
+# do uboot specific stuff
+(cd $BOOTLOADERDIR/uboot; \
+  REL=$(git tag -l rel* | head -n1); \
+  echo "Building $REL"; \
+  git checkout $REL)
+
+# update the qts files
+echo "Updating QTS files for $BOARD"
+$QTSFILTER \
+  cyclone5 \
+  $HWDIR/ \
+  $BOOTLOADERDIR/ \
+  $BOOTLOADERDIR/uboot/board/$BOARD/qts/
+
+echo "Building uboot"
+make -C $BOOTLOADERDIR/uboot distclean
+make -C $BOOTLOADERDIR/uboot $UBOOT_CFG 
+make -C $BOOTLOADERDIR/uboot -j$(nproc)
