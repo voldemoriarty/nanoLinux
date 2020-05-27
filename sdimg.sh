47beb4d861097b2e0f9f8b049df3ac13306b6a98
voldemoriarty <mamin.bee16seecs@seecs.edu.pk>
2020-05-27 13:40:41 +0500

    change back to ext3

--- sdimg.sh
+++ sdimg.sh
@@ -7,6 +7,6 @@ echo "====================="
 sudo ./make_sdimage_p3.py -f \
 -P bootloader/binaries/u-boot-with-spl.sfp,num=3,format=raw,size=10M,type=A2  \
 -P sdfs,num=1,format=vfat,size=200M \
--P rootfs,num=2,format=ext4,size=500M \
+-P rootfs,num=2,format=ext3,size=500M \
 -s 720M \
 -n sdcard_cv.img
